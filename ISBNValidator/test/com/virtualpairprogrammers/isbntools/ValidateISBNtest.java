1a220049aba78760a9f18ea270efb852f470c02b
Daniel Mastain <dmastain@gmail.com>
2020-09-25 20:24:17 -0500

    commit of first fail test

---
+++ ISBNValidator/test/com/virtualpairprogrammers/isbntools/ValidateISBNtest.java
@@ -0,0 +1,14 @@
+package com.virtualpairprogrammers.isbntools;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ValidateISBNtest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
